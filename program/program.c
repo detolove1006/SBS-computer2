0d0ccb17087ff7adefbbf294b4f5bed70f9c3500
detolove1006 <158586434+detolove1006@users.noreply.github.com>
2025-08-18 19:49:57 +0900

    2025 8.18 키보드 시스템

--- program/program.c
+++ program/program.c
@@ -1,40 +1,45 @@
 ﻿#include <stdio.h>
 
-#define SIZE 1000
 
+#define SIZE 10
 
 
 int main()
 {
 
-#pragma region 파일 입출력
+#pragma region GetAsyncKeyState
+	// windows API에서 제공하는 입력 처리 함수로, 지정된 키의 상태를
+	// 비동적으로 확인할 때 사용되는 함수입니다.
+
+
+	
+	const char* dialog[SIZE];
+
+	dialog[0] = "의뢰인 : 잘못을 크게 저질렀습니다.";
+	dialog[1] = "탐정 : 무슨 잘못을 저질렀나요?";
+	dialog[2] = "의뢰인 :사람을 흉기로 찔렀어요 .";
+	dialog[3] = "탐정 : 자수하시는 건가요?";
+	dialog[4] = "의뢰인 : 넵..";
+	dialog[5] = "탐정 : 현장에서 당신의 dna가 안나왔습니다";
+	dialog[6] = "의뢰인 : ...";
+	dialog[7] = "탐정 : 거짓말을 하시고있나요?";
+	dialog[8] = "의뢰인 : 아뇨";
+	dialog[9] = "탐정 : 거짓말을 하시면 안됩니다.";
 	
 
 
 
-
-	// fopen(첫 번째 매개변수) : 파일의 이름
-	// fopen(두 번째 매개 변수) : 입력 & 출력모드
-
 	
-	// "r" : 읽기 전용 (파일이 없을 때 : NULL)
-	// "w" : 쓰기 전용 (새로 파일을 생성)
-	// "a" : 추가 모드 (새로운 파일을 생성 - 기존에 있는 파일에 내용을 추가)
-
 	
-	//FILE* file = fopen("data.txt", "w");
-	//
-	//fputs("strength : \n", file);
-	//fputs("dexterity : \n", file);
-	//fputs("wisdom : \n", file);
-	//fputs("intelligence : \n", file);
-	//fputs("constitution : \n", file);
-	//
-	//fclose(file);
+	// key state
+	// 0x0000 :이전에 누른 적이 없고, 호출 시점에도 눌러있지 않은 상태
 
+	// 0x0001 : 이전에 누른 적이 있고, 호출 시점에는 눌려있지 않은 상태
 
+	// 0x8000 : 이전에 누른 적이 없고, 호출 시점에는 눌려있는 상태
 
-	
+	//  0x8001 : 이전에 누른 적이 있고, 호출 시점에는 눌려있는 상태
+
 
 
 
@@ -42,35 +47,8 @@ int main()
 #pragma endregion
 
 
-#pragma region File Write
-
-#pragma endregion
-
-#pragma region File Read
-
-	FILE* file = fopen("data.txt", "r");
-
-	char buffer[SIZE] = { 0, };
-
-	// 1. buffer : 읽을 데이터를 저장할 메모리
-	// 2. element size : 각 데이터 항목의 크기
-	// 3. element count : 데이터를 읽어올 데이터 항목의 수
-	// 4. stream : 데이터를 읽어올 파일 포인터
-
-	fread(buffer, 1, SIZE, file);
-
-	printf("%s", buffer);
-
-	
-
-	fclose(file);
 
 
-
-#pragma endregion
-
-
-//pull request
 
 
 	return 0;
